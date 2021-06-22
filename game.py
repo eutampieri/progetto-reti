66ecf9c744f5571800133bd154ead7b6ec570d1d
MyK_00L <michaelchelli00@gmail.com>
2021-06-22 11:44:46 +0200

    added game

---
+++ game.py
@@ -0,0 +1,36 @@
+import json
+import random
+from http_client import *
+
+url = ("raw.githubusercontent.com","/deepmind/AQuA/master/test.json")
+
+#questions = [json.loads(i) for i in open("test.json").read().splitlines()]
+questions = [json.loads(i) for i in https_get(url[0],url[1]).splitlines()]
+
+# random.seed(42)
+
+def get_random_question():
+	question_num = random.randint(0,len(questions)-1)
+	correct_answer = ord(questions[question_num]["correct"]) - ord("A")
+	choices = [0,1,2,3,4]
+	choices.remove(correct_answer)
+	choices = random.choices(choices,k=2)
+	choices.append(correct_answer)
+	random.shuffle(choices)
+	correct_answer = choices.index(correct_answer)
+	answers = [questions[question_num]["options"][i][2:] for i in choices]
+	question = questions[question_num]["question"]
+	rationale = questions[question_num]["rationale"]
+	rationale = rationale[:rationale.rfind('\n')]
+	return question, answers, correct_answer, rationale
+
+cache = []
+def get_question(turn, num):
+	assert isinstance(turn,int) and isinstance(num,int), "inputs should be int"
+	assert num==0 or num==1, "num should be either 0 or 1"
+	assert turn>=0, "turn should be >=0"
+	ind = turn*2+num
+	while ind>=len(cache):
+		cache.append(get_random_question())
+	return cache[ind]
+
